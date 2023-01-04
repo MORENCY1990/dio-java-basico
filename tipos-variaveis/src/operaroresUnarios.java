8afeef70317625a72bcda641158e70edf4d75c42
Morency <morencycastanheira27@gmail.com>
2023-01-04 20:05:56 -0300

    OPERADORES

---
+++ tipos-variaveis/src/operaroresUnarios.java
@@ -0,0 +1,14 @@
+public class operaroresUnarios {
+    public static void main(String[] args) {
+        
+        int numero = 5;
+
+        // ou numero = - numero;
+        // numero = + numero; 
+        // numero = numero * -1; RETORNA POSITIVO
+
+        System.out.println(numero);
+        System.out.println(-numero);
+
+    }
+}
